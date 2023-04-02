c0a6f6f7f84598783930e7f9887e0dee2b4924a1
Tushar Dhiman <tusharscoderoom@gmail.com>
2023-04-02 11:30:22 +0530

    Revert "react project first commit"
    This reverts commit 4e3c01692c094c03d758e33faada3cfe865013ce.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
